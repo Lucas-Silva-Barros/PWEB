6ff563699b4458cdd92e4b21ebd1dad07529de9f
Lucas-Silva-Barros <69805261+Lucas-Silva-Barros@users.noreply.github.com>
2020-10-26 15:38:12 -0300

    Add files via upload

---
+++ Atividade10/Atividade10.js
@@ -0,0 +1,11 @@
+function Abrir() {
+    document.getElementById("Imagem").src = "Janela Fechada.jpg";
+}
+
+function Aberta() {
+    document.getElementById("Imagem").src = "Janela Aberta.jpg";
+}
+
+function Quebrar() {
+    document.getElementById("Imagem").src = "Janela Quebrada.jpg";
+}
